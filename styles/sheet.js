c5683036572305f6b9ff92317beb7a6322707800
nifredy <fredydominic0@yahoo.com>
2021-04-09 19:22:09 +0200

    learning responsiveness

---
+++ styles/sheet.js
@@ -0,0 +1,16 @@
+console.log('hello jessus');
+ //alert ('yooo');
+ //var name = prompt('what is your name?');
+
+//  document.getElementById('someText').innerHTML = name;
+
+ function greetings(yourName) {
+     var result= 'hello' +' '+ yourName;
+     console.log(result);
+
+ }
+ var yourName = prompt('what is your name?');
+ greetings(yourName);
+
+ 
+  
